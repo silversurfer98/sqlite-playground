6105af4329fa06e0baab502d0ede083d3edadf53
RAGHAVENDAR SRIDHAR <RAGHAVENDAR.SRIDHAR@saipem.com>
2022-08-04 15:51:28 +0530

    database connection working / dll method not working

--- test.cpp
+++ test.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <sqlite3.h>
 int main()
 {
-    return 0;
+    std::cout<<"skeleton baby\n";
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if( rc ) {
+       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+       return(0);
+    } else {
+       fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
 }
