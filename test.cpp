1c928badcfeeb096a9436d7b2ac37070a6ec2bc4
RAGHAVENDAR SRIDHAR <RAGHAVENDAR.SRIDHAR@saipem.com>
2022-08-04 16:26:33 +0530

    db open working

--- test.cpp
+++ test.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
 #include <sqlite3.h>
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) 
+{
+   int i;
+   for(i = 0; i<argc; i++) 
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+   printf("\n");
+   return 0;
+}
+
+bool opendb(sqlite3 *db, const char* filename)
+{
+    int rc;
+    rc = sqlite3_open(filename, &db);
+
+    if(rc) 
+    {
+       std::cout<<"Can't open database: %s\n"<<sqlite3_errmsg(db);
+       return false;
+    } 
+    else 
+    {
+       std::cout<<"Opened database successfully\n";
+       return true;
+    }
+}
 int main()
 {
-    std::cout<<"skeleton baby\n";
+    //std::cout<<"skeleton baby\n";
     sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
+    if(opendb(db,"test.db"))
+    {
+        std::cout<<"\n\nomale\n\n";
+        // --------------------------------
+        /* Create SQL statement */
+   const char* sql = "CREATE TABLE mynumbers("  \
+      "ID INT PRIMARY KEY   NOT NULL," \
+      "value    INT );";
 
-    rc = sqlite3_open("test.db", &db);
-
-    if( rc ) {
-       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-       return(0);
-    } else {
-       fprintf(stderr, "Opened database successfully\n");
+char *zErrMsg = 0;
+int rc;
+rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Table created successfully\n");
+   }
+        // ---------------------------------
+        sqlite3_close(db);
     }
-    sqlite3_close(db);
+    return 0;
+    
 }
